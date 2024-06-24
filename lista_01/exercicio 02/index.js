93b9ce6f203595a1a092a56e99a267f8a215633e
Daniel Ferreira <daniel_ferreira12@estudante.sesisenai.org.br>
2024-06-24 19:57:01 -0300

    contador exercicio 2

---
+++ lista_01/exercicio 02/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador de Cliques</title>
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <button onclick="incrementarContador()">Clique aqui</button>
+    <p>Número de cliques: <span id="contador">0</span></p>
+</body>
+</html>
