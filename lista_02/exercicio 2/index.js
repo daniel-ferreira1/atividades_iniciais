6e95f58a7e3a58b34c0f4fa1ab4abd56c3fecde0
Daniel Ferreira <daniel_ferreira12@estudante.sesisenai.org.br>
2024-07-03 20:58:18 -0300

    ATV 02

--- lista_02/exercicio 2/index.js
+++ lista_02/exercicio 2/index.js
@@ -5,27 +5,27 @@ document.getElementById('formulario').addEventListener('submit', function(event)
     var email = document.getElementById('email').value.trim();
     var mensagemElement = document.getElementById('mensagem');
 
-    // Resetar mensagem de erro
+    
     mensagemElement.textContent = '';
 
-    // Validar campo Nome
+    
     if (nome === '') {
         mensagemElement.textContent = 'Por favor, preencha o nome.';
-        return; // Impede o envio do formulário se o nome estiver vazio
+        return; 
     }
 
-    // Validar campo Email
+    
     if (email === '') {
         mensagemElement.textContent = 'Por favor, preencha o email.';
-        return; // Impede o envio do formulário se o email estiver vazio
+        return; 
     }
 
-    // Se os campos estiverem preenchidos corretamente, exibir os dados no console em formato JSON
+   
     var dados = {
         nome: nome,
         email: email
     };
     console.log('Dados do formulário:', JSON.stringify(dados));
 
-    // Aqui você pode enviar os dados para o servidor ou realizar outra ação desejada após a validação
+   
 });
