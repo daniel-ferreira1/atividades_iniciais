0f4bee5af2ccccb2ade07ca053255833936161ad
Daniel Ferreira <daniel_ferreira12@estudante.sesisenai.org.br>
2024-08-05 21:23:20 -0300

    exercicio 3

--- lista_04/exercicio 3/atividade.php
+++ lista_04/exercicio 3/atividade.php
@@ -1,23 +1,14 @@
 <?php
 
-function ehPar($numero) {
-
-    return ($numero % 2 == 0) ? 1 : 0;
-}
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $numero = isset($_POST['numero']) ? (int)$_POST['numero'] : 0;
-
-    
-    $resultado = ehPar($numero);
-
-   
-    if ($resultado == 1) {
-        echo "O número $numero é par.";
-    } else {
-        echo "O número $numero é ímpar.";
+class MinhaClasse {
+    function ehPar($a) {
+        return $a % 2 == 0;
     }
 }
-?>
+
+$minhaClasse = new MinhaClasse();
+$resultado = 'o numero é impar';
+if ($minhaClasse->ehPar($_GET['a'])){
+    $resultado = 'o numero é par';
+}
+echo($resultado);
--- lista_04/exercicio 3/index.js
+++ lista_04/exercicio 3/index.js
@@ -1,26 +1,12 @@
-document.getElementById('botao').addEventListener('click', function() {
-    
-    const numero = document.getElementById('numero').value;
+let botao = document.getElementById('botao')
 
-  
-    if (numero === '' || isNaN(numero)) {
-        document.getElementById('mensagem').innerText = 'Por favor, insira um número válido.';
-        return;
-    }
+botao.addEventListener('click', function() {
+    let a = document.getElementById('a').value
+   
 
-    
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'verificar_paridade.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            
-            document.getElementById('mensagem').innerText = xhr.responseText;
-        }
-    };
-
-    
-    xhr.send('numero=' + encodeURIComponent(numero));
-});
+    fetch('atividade.php?a=' + a ).then(function(resposta) {
+        return resposta.text();
+    }).then(function(texto) {
+        document.getElementById('mensagem').innerText = texto
+    })
+})
